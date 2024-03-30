37c3e15679139a1fb0822cb4fb1aaa6a833e5b36
Lucas Chipolari <zlucaschipolariz@gmail.com>
2024-03-30 11:43:14 -0300

    Di formato al ejercicio 4 y realice el ejercicio 5

--- E4/index.js
+++ E4/index.js
@@ -2,24 +2,22 @@
 número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con
 “cancelar” deberá indicarse la suma total de los números introducidos.*/
 
-let suma=0;
-do{
-    let numero = prompt('Ingrese un numero');
-    if(numero !== null){
-        if(numero.trim().length>0){
-            if(!isNaN(numero)){
-                numero=Number(numero);
-                suma += numero;
-            }else{
-                alert('Ingrese un numero valido');
-            }
-        }    
-
-    }else{
-        if(confirm('Desea terminar?')){
-            alert(suma);
-            break;
-        }
+let suma = 0;
+do {
+  let numero = prompt("Ingrese un numero");
+  if (numero !== null) {
+    if (numero.trim().length > 0) {
+      if (!isNaN(numero)) {
+        numero = Number(numero);
+        suma += numero;
+      } else {
+        alert("Ingrese un numero valido");
+      }
     }
-
-}while(true);
+  } else {
+    if (confirm("Desea terminar?")) {
+      alert(suma);
+      break;
+    }
+  }
+} while (true);
