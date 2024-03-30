f74548323d023e3794f8c06e624787a5e379606f
Lucas Chipolari <zlucaschipolariz@gmail.com>
2024-03-30 11:25:53 -0300

    termine el ejercicio 4

---
+++ E4/index.js
@@ -0,0 +1,25 @@
+/*4- Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un
+número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con
+“cancelar” deberá indicarse la suma total de los números introducidos.*/
+
+let suma=0;
+do{
+    let numero = prompt('Ingrese un numero');
+    if(numero !== null){
+        if(numero.trim().length>0){
+            if(!isNaN(numero)){
+                numero=Number(numero);
+                suma += numero;
+            }else{
+                alert('Ingrese un numero valido');
+            }
+        }    
+
+    }else{
+        if(confirm('Desea terminar?')){
+            alert(suma);
+            break;
+        }
+    }
+
+}while(true);
